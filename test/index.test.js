7be1fe2653e9697e7dd28325899f9b7d28eb7e9c
dushimeemma <dushimeemma@gmail>
2021-01-27 12:49:43 +0200

    chore(setup): setup node/express project

---
+++ test/index.test.js
@@ -0,0 +1,34 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+import app from '../index';
+
+chai.use(chaiHttp);
+chai.should();
+
+describe('App', () => {
+  it('Should Get The Landing Page', (done) => {
+    chai
+      .request(app)
+      .get('/')
+      .end((err, res) => {
+        if (err) done(err);
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('message');
+        done();
+      });
+  });
+  it('Should not Get Invalid route', (done) => {
+    chai
+      .request(app)
+      .get('/invalid')
+      .end((err, res) => {
+        if (err) done(err);
+        res.should.have.status(404);
+        res.body.should.be.a('object');
+        res.body.should.have.property('message');
+        done();
+      });
+  });
+});
